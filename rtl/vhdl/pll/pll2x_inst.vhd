1c35703e61c9d7b9329d37d83854a076eb7a446e
chromatose <chromatose@4274e12e-711e-0410-a7fc-e7745ff7e89c>
2002-04-21 19:32:50 +0000

    Submitted by Benj Carson:
    Removed uneccesary files.
    
    
    git-svn-id: svn://svn.icculus.org/manticore/trunk@6 4274e12e-711e-0410-a7fc-e7745ff7e89c

--- rtl/vhdl/pll/pll2x_inst.vhd
+++
@@ -1,5 +0,0 @@
-pll2x_inst : pll2x PORT MAP (
-		inclock	 => inclock_sig,
-		clock0	 => clock0_sig,
-		clock1	 => clock1_sig
-	);
--- rtl/vhdl/rasterizer/raster_ram_temp_inst.vhd
+++
@@ -1,9 +0,0 @@
-raster_ram_temp_inst : raster_ram_temp PORT MAP (
-		data	 => data_sig,
-		wraddress	 => wraddress_sig,
-		rdaddress	 => rdaddress_sig,
-		wren	 => wren_sig,
-		rden	 => rden_sig,
-		clock	 => clock_sig,
-		q	 => q_sig
-	);
